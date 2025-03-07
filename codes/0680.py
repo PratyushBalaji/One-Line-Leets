d36aa5ff3d6503631e935975d0335d1ce61badb3
Pratyush <balajipratyush@gmail.com>
2025-03-07 00:55:11 -0500

    Solution 680 (TLE) - Attempted using generator, solves mle issue but now tles

--- codes/0680.py
+++ codes/0680.py
@@ -4,7 +4,10 @@
 
 class Solution(object):
     def validPalindrome(self, s):
-        return True in map(lambda x:x == x[::-1], [s[:i]+s[i+1:] for i in range(len(s))] + [s])
+        # return True in map(lambda x:x == x[::-1], [s[:i]+s[i+1:] for i in range(len(s))] + [s])
+        return next((True for i in range(len(s)) if s[:i] + s[i+1:] == (s[:i] + s[i+1:])[::-1]), s == s[::-1])
+
     
     
-# Works in theory, but exceeds memory limit in practice since it is essentially O(n^2)
+# Solution 1 works in theory, but exceeds memory limit in practice since it is essentially O(n^2)
+# Solution 2 works in theory, and optimises memory by using a generator, but exceeds time limit in practice
