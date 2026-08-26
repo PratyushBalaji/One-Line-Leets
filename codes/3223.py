978dd2bb2671f873fd5223155e4947097ef0c600
Pratyush Balaji <59258572+PratyushBalaji@users.noreply.github.com>
2026-08-26 09:55:49 -0400

    soln: 3223

---
+++ codes/3223.py
@@ -0,0 +1,12 @@
+# You are given a string s.
+# You can perform the following process on s any number of times:
+#     Choose an index i in the string such that there is at least one character to the left of index i that is equal to s[i], and at least one character to the right that is also equal to s[i].
+#     Delete the closest occurrence of s[i] located to the left of i.
+#     Delete the closest occurrence of s[i] located to the right of i.
+# Return the minimum length of the final string s that you can achieve.
+
+# https://leetcode.com/problems/minimum-length-of-string-after-operations/
+
+class Solution(object):
+    def minimumLength(self, s):
+        return sum((((s.count(i) + 1) & 1) + 1 for i in set(list(s))))
